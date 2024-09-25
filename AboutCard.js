543051c1e9efc29cd34bd6f9b2152d83e48f8962
Sachin Mahajan <60482896+sachinmahajan817@users.noreply.github.com>
2024-09-25 16:44:04 +0530

    Update AboutCard.js

--- AboutCard.js
+++ AboutCard.js
@@ -8,13 +8,12 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Sachin Mahajan </span>
+            from <span className="purple"> Pune, India.</span>
             <br />
             I am currently employed as a software developer at Juspay.
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            I have completed Integrated MCA  in Computing at D.Y.Patil Vidhyapeeth
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -34,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Sachin_Mahajan</footer>
         </blockquote>
       </Card.Body>
     </Card>
